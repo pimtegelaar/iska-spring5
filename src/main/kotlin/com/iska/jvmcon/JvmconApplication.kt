8438ca85ca9e498e6ff25c09521573b25c34d127
pimtegelaar <pimtegelaar@gmail.com>
2017-12-03 20:01:12 +0100

    initial commit

---
+++ src/main/kotlin/com/iska/jvmcon/JvmconApplication.kt
@@ -0,0 +1,11 @@
+package com.iska.jvmcon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JvmconApplication
+
+fun main(args: Array<String>) {
+    runApplication<JvmconApplication>(*args)
+}
